f3e77a56fd8f382851d3ac8d8e3fdf10d611d42a
Malachi Griffith <mgriffit@genome.wustl.edu>
2018-05-01 16:26:20 -0500

    add some collaborator volumes

--- misc/summarize_disk_space.pl
+++ misc/summarize_disk_space.pl
@@ -14,8 +14,10 @@ my @dirs = qw (/gscmnt/gc2142/griffithlab/
                /gscmnt/gc2607/griffithlab/ 
                /gscmnt/gc2543/griffithlab/
                /gscmnt/gc2736/griffithlab_gms/
-               /gscmnt/gc2753/fehniger/
                /gscmnt/gc2596/griffith/
+               /gscmnt/gc2753/fehniger/
+               /gscmnt/gc2624/fehniger/
+               /gscmnt/gc2552/uppaluri/
                );
 
 my $grand_total_allocated_tb = 0;
