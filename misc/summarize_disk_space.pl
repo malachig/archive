1300f0fa0422ff8d3cf4f9e88165d1e8c29e386d
Malachi Griffith <mgriffit@genome.wustl.edu>
2018-11-27 15:00:42 -0600

    add cyclinE disk

--- misc/summarize_disk_space.pl
+++ misc/summarize_disk_space.pl
@@ -18,6 +18,8 @@ my @dirs = qw (/gscmnt/gc2142/griffithlab/
                /gscmnt/gc2753/fehniger/
                /gscmnt/gc2624/fehniger/
                /gscmnt/gc2552/uppaluri/
+               /gscmnt/gc2609/gbm_antigen_dunn/
+               /gscmnt/gc2606/griffith/
                );
 
 my $grand_total_allocated_tb = 0;
