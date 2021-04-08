0e0500450dae5ac3d8348906e3b69782bf11eb52
Malachi Griffith <malachig@gmail.com>
2021-04-08 16:19:26 -0500

    minor update

--- misc/filterLargeIntronAlignments.sh
+++ misc/filterLargeIntronAlignments.sh
@@ -1,9 +1,14 @@
-#sub -i 'docker(zlskidmore/samtools:1.10)'
-#filterLargeIntronAlignments.sh /storage1/fs1/mgriffit/Active/scrna_mcb6c/Mouse_Bladder_MCB6C_Arora/scRNA/Rep3/v5/regtools/ Rep3_Control_tumor.bam /scratch1/fs1/mgriffit/filtered_scrna_bams/
+#!/bin/bash
+#isub -i 'docker(zlskidmore/samtools:1.10)'
+#filterLargeIntronAlignments.sh /storage1/fs1/mgriffit/Active/scrna_mcb6c/Mouse_Bladder_MCB6C_Arora/scRNA/Rep3/v5/regtools/ Rep3_Control_tumor.bam /scratch1/fs1/mgriffit/filtered_scrna_bams/bams
 
 indir=$1
 filename=$2
-outdir=$2
+outdir=$3
+
+echo $indir
+echo $filename
+echo $outdir
 
 #create BAM with intron alignments >50000 removed
 samtools view -h $indir/$filename | perl -ne 'chomp; @l=split("\t",$_); if($l[5] =~ /(\d+)N/){if($1<50000){print "$_\n"}}else{print "$_\n"}' 2>/dev/null | samtools view -bS - > $outdir/$filename
