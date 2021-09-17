000545542065a7ddb3e3a1c7a6e5ef6db0e00a6c
Malachi Griffith <malachig@gmail.com>
2021-09-17 16:07:07 -0500

    fix removal of multi intron alignments

--- misc/filterLargeIntronAlignments.sh
+++ misc/filterLargeIntronAlignments.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #isub -i 'docker(zlskidmore/samtools:1.10)'
-#~/git/archive/misc/filterLargeIntronAlignments.sh /storage1/fs1/mgriffit/Active/scrna_mcb6c/Mouse_Bladder_MCB6C_Arora/scRNA/Rep1/v5/M_HC-JKB216-ICB_MCB6C_Rep_1-lib1/outs/ possorted_genome_bam.bam /scratch1/fs1/mgriffit/tmp/process/ /scratch1/fs1/mgriffit/tmp/regions.txt /scratch1/fs1/mgriffit/tmp/out/
+#~/git/archive/misc/filterLargeIntronAlignments.sh /storage1/fs1/mgriffit/Active/scrna_mcb6c/Mouse_Bladder_MCB6C_Arora/scRNA/Rep1/v5/M_HC-JKB216-ICB_MCB6C_Rep_1-lib1/outs/ possorted_genome_bam.bam /scratch1/fs1/mgriffit/tmp/process/ /scratch1/fs1/mgriffit/tmp/regions.txt /storage1/fs1/mgriffit/Active/tmp/ 5000
+
 #regions format perline: chr16:33952785-33969003
 
 indir=$1
@@ -8,12 +9,16 @@ filename=$2
 scratchdir=$3
 regions=$4
 finaldir=$5
+maxsize=$6
 
-echo $indir
-echo $filename
-echo $scratchdir
-echo $regions
-echo $finaldir
+echo Input dir: $indir
+echo BAM file name: $filename
+echo Working dir: $scratchdir
+echo Regions list file: $regions
+echo Output dir: $finaldir
+echo Max intron size: $maxsize
+
+export MAXSIZE=$maxsize
 
 #create BAM covering only the regions of interest
 cat $regions | xargs samtools view -b $indir/$filename > $scratchdir/$filename.subset.bam
@@ -22,8 +27,8 @@ cat $regions | xargs samtools view -b $indir/$filename > $scratchdir/$filename.s
 samtools sort -O bam -o $scratchdir/$filename.subset.sort.bam $scratchdir/$filename.subset.bam 
 mv $scratchdir/$filename.subset.sort.bam $scratchdir/$filename.subset.bam
 
-#create BAM with intron alignments >50000 removed
-samtools view -h $scratchdir/$filename.subset.bam | perl -ne 'chomp; @l=split("\t",$_); if($l[5] =~ /(\d+)N/){if($1<50000){print "$_\n"}}else{print "$_\n"}' 2>/dev/null | samtools view -bS - > $scratchdir/$filename
+#create BAM with intron alignments greater than specified size removed
+samtools view -h $scratchdir/$filename.subset.bam | perl -ne 'chomp; $maxlength=0; @l=split("\t",$_); if($l[5] =~ /(\d+)N/){@ops=split(/\d+/,$l[5]); shift @ops; @lengths=split(/\D+/, $l[5]); foreach $op(@ops){$length=shift(@lengths); if($op eq "N" && $length>$maxlength){$maxlength=$length}}; if($maxlength<$ENV{MAXSIZE}){print "$_\n"}}else{print "$_\n"}' 2>/dev/null | samtools view -bS - > $scratchdir/$filename
 
 #add header back to BAM
 samtools view -H $indir/$filename > $scratchdir/$filename.header
